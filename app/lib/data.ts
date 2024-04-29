f52414d5ac2580785d60d58e3b04f356ba7d29e4
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2024-04-29 10:50:33 +0530

    auth added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -169,8 +169,8 @@ export async function fetchInvoiceById(id: string) {
 
     return invoice[0];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch invoice.');
   }
 }
 
